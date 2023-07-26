d7d34bcfebd24fd9fc95ff83436ec15b675ebf5f
michael7nightingale <suslanchikmopl@gmail.com>
2023-07-26 23:11:35 +0500

    update repository logic

--- lawyer/repositories.py
+++ lawyer/repositories.py
@@ -1,19 +1,47 @@
 from sqlalchemy import Table
 from aiopg.sa import Engine
 from contextlib import asynccontextmanager
+from abc import ABC, abstractmethod
 
-from db import message
+
+class BaseRepository(ABC):
+    @abstractmethod
+    def __init__(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def create(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def get(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def filter(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def delete(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def all(self, *args, **kwargs):
+        pass
 
 
-class BaseRepository:
-    model: Table
-
-    def __init__(self, db: Engine):
+class AiopgRepository(BaseRepository):
+    def __init__(self, db: Engine, model: Table):
         self._db = db
+        self._model = model
 
     @property
     def db(self) -> Engine:
         return self._db
+
+    @property
+    def model(self) -> Table:
+        return self._model
 
     @asynccontextmanager
     async def connection(self):
@@ -24,6 +52,33 @@ class BaseRepository:
         async with self.connection() as conn:
             await conn.execute(self.model.insert().values(**values))
 
+    async def get(self, *args, **kwargs):
+        if len(args) == 1:
+            kwargs.update(id=args[0])
+        else:
+            raise ValueError("Kwargs expected")
+        async with self.connection() as conn:
+            expected = (
+                getattr(self.model, k) == v for k, v in kwargs.items()
+            )
+            cur = await conn.execute(self.model.select().where(*expected))
+            result = await cur.fetchone()
+            return result
 
-class MessageRepository(BaseRepository):
-    model = message
+    async def delete(self, id_):
+        async with self.connection() as conn:
+            await conn.execute(self.model.delete(self.model.id == id_))
+
+    async def all(self):
+        async with self.connection() as conn:
+            cur = await conn.execute(self.model.select())
+            return await cur.fetchall()
+
+    async def filter(self, **kwargs):
+        async with self.connection() as conn:
+            expected = (
+                getattr(self.model, k) == v for k, v in kwargs.items()
+            )
+            cur = await conn.execute(self.model.select().where(*expected))
+            result = await cur.fetchall()
+            return result
--- lawyer/services.py
+++ lawyer/services.py
@@ -1,18 +1,30 @@
 from aiopg.sa import Engine
+from sqlalchemy import Table
 from typing import Type
+from abc import ABC, abstractmethod
 
-from repositories import BaseRepository, MessageRepository
+from db import Message
+from repositories import BaseRepository, AiopgRepository
 
 
-class BaseService:
+class BaseService(ABC):
     repository_class: Type[BaseRepository]
 
+    @abstractmethod
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class AiopgService(BaseService):
+    repository_class = AiopgRepository
+    model: Table
+
     def __init__(self, db: Engine):
-        self.repository = self.repository_class(db)
+        self.repository = self.repository_class(db=db, model=self.model)
 
 
-class MessageService(BaseService):
-    repository_class = MessageRepository
+class MessageService(AiopgService):
+    model = Message
 
     async def create_message(self, **kwargs):
         await self.repository.create(**kwargs)
