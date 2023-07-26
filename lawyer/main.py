eafa771a565c782b06fe036d190fc2759212d3f9
michael7nightingale <suslanchikmopl@gmail.com>
2023-07-26 13:26:31 +0500

    create aiohttp application

---
+++ lawyer/main.py
@@ -0,0 +1,31 @@
+from aiohttp import web
+import aiohttp_jinja2
+import jinja2
+
+from db import pg_context
+from routes import setup_routes
+from settings import config, BASE_DIR
+
+
+application = web.Application()
+application['config'] = config
+aiohttp_jinja2.setup(
+    app=application,
+    loader=jinja2.FileSystemLoader(str(BASE_DIR / "lawyer" / "templates"))
+)
+
+
+def setup_static_routes(app: web.Application):
+    app.router.add_static(
+        prefix="/static",
+        path=BASE_DIR / "lawyer" / "static",
+        name='static'
+    )
+
+
+setup_routes(application)
+setup_static_routes(application)
+application.cleanup_ctx.append(pg_context)
+web.run_app(
+    app=application,
+)
